db1ccdbe5a2cf5fa840cb26651ca14342f8bf528
alohr51 <Andrew.lohr@yahoo.com>
2014-02-05 16:49:44 -0500

    Created .sql file
    Realized that the hand in must be in a .sql file so I redid the lab
    making sure to keep SELECT, FROM, and WHERE on their own lines for easy
    grading. I saved the file directly from the query editor and named the
    file Lab3.sql and placed it in the Lab3 folder. I also deleted all the
    screenshots from the previous commit.

---
+++ Lab 3/Lab3.sql
@@ -0,0 +1,36 @@
+﻿--Q1
+select name, city 
+from agents 
+where name = 'Smith'
+--Q2
+select pid, name, quantity
+from products
+where priceusd > 1.25
+--Q3
+select ordno, aid 
+from orders
+--Q4
+select name, city 
+from customers 
+where city = 'Dallas'
+--Q5
+select *
+from agents 
+where city != 'New York' and city != 'Newark'
+--Q6
+select *
+from products 
+where city != 'New York' and city != 'Newark' and priceusd >= 1.00
+--Q7
+select *
+from orders 
+where mon = 'jan' or mon = 'mar'
+--Q8
+select *
+from orders 
+where mon = 'feb' and dollars < 100.00
+--Q9
+select *
+from orders 
+where cid = 'c001'
+
