c508599f9366801b70f56b4885b8de2db7d92d1b
alohr51 <Andrew.lohr@yahoo.com>
2014-02-20 00:39:52 -0500

    Lab5 Upload
    Refreshed the cap2 database for this assignment just in case. Question 2
    was the toughest. I could not get the duplicate pids of the 3 agents,
    only the pid of the 3 respective agents would return. #7 was not bad,
    but for some reason it took me the longest. It was one of those times
    when you just really over think something and make it way more
    complicated than it had to be. Came back after a break and solved it in
    5 minutes...

---
+++ Lab 5/Lab5.sql
@@ -0,0 +1,80 @@
+﻿--Andrew Lohr
+--Lab 5
+
+--Q1
+select agents.city
+from agents
+inner join orders
+    on agents.aid = orders.aid
+inner join customers
+    on customers.cid = orders.cid
+    where customers.name = 'Basics'
+
+--Q2 needs to be more than 3 results. It won't select all the pids from orders with
+--those agents, only their respective ones.
+select pid
+from orders
+inner join agents
+	on orders.aid = agents.aid
+inner join customers
+	on customers.cid = orders.cid
+	where customers.city = 'Kyoto'
+
+--Q3
+select name
+from customers
+where cid
+not in
+(select cid
+from orders)
+
+--Q4
+select name 
+from customers
+left outer join orders
+	on customers.cid = orders.cid
+	where orders.cid is null
+
+--Q5 tiptop orderd twice through otasi, 
+--just remove the groupby to see the duplicate entry
+select customers.name, agents.name 
+from customers
+inner join orders
+	on customers.cid = orders.cid
+inner join agents
+	on customers.city = agents.city
+	and agents.aid = orders.aid
+	group by customers.name,agents.aid
+
+--Q6
+select customers.name, agents.name, agents.city
+from agents
+inner join customers
+	on customers.city = agents.city
+
+--Q7
+select name, city
+from customers
+where city
+in
+(select city
+from products
+where quantity 
+in
+(select min(quantity)
+from products))
+
+
+	
+
+
+
+	
+	
+
+
+
+
+
+
+
