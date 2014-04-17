766f72ec66698f39af894d0a17db055a1ecab4d8
alohr51 <Andrew.lohr@yahoo.com>
2014-04-16 23:57:25 -0400

    Lab10
    Lab10 initial commit

---
+++ Lab10/Lab10.sql
@@ -0,0 +1,34 @@
+﻿--Andrew Lohr--
+
+--Drop PreReqsFor--
+DROP FUNCTION PreReqsFor(integer);
+
+--Create--
+CREATE OR REPLACE FUNCTION PreReqsFor(courseNumber integer)
+RETURNS SETOF integer AS $$
+	Select prereqnum
+	from Prerequisites
+	where coursenum = courseNumber;
+
+$$ LANGUAGE sql;
+
+--Query Test--
+select PreReqsFor(499);
+
+
+
+--Drop IsPreReqsFor--
+DROP FUNCTION IsPrereqsFor(integer);
+
+--Create--
+CREATE OR REPLACE FUNCTION IsPreReqsFor(courseNumber integer)
+RETURNS SETOF integer AS $$
+
+	Select coursenum
+	from Prerequisites
+	where prereqnum = courseNumber;
+
+$$ LANGUAGE sql;
+
+--Query Test--
+select IsPreReqsFor(220);
